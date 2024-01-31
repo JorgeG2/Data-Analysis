7262dc61d8f089cba607e49cdfb51ee04283516a
JorgeG2 <Jorgegonzales191421@Gmail.com>
2024-01-31 14:43:27 -0500

    deleted stuff

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jorgegonzales/Desktop/Main Problem Set/env"
+set -gx VIRTUAL_ENV "/Users/jorgegonzales/Desktop/Data-Analysis/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
